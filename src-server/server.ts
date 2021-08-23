c0316d9e8dbc3aeeb586a993fcea52aa497c89e7
RFalowo <Oluremifalowo@gmail.com>
2021-08-23 16:01:18 +0100

    adnas

--- src-server/server.ts
+++ src-server/server.ts
@@ -13,15 +13,15 @@ dotenv.config();
 // dotenv.config({ path: "app/.env" });
 // const uri = process.env.MONGODBCRED;
 // console.log(uri);
-// mongoose.connect(
-//   "mongodb+srv://Remi:TJQvAr9SnEDGU2D@cluster0.43i0s.mongodb.net/Thesis?retryWrites=true&w=majority",
-//   {
-//     keepAlive: true,
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useFindAndModify: false,
-//   }
-// );
+mongoose.connect(
+  "mongodb+srv://Remi:TJQvAr9SnEDGU2D@cluster0.43i0s.mongodb.net/Thesis?retryWrites=true&w=majority",
+  {
+    keepAlive: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  }
+);
 
 // setTimeout(() => {
 //   UserModel.estimatedDocumentCount().exec((err, count) => {
@@ -58,18 +58,6 @@ const clients: clients[] = [];
 
 wsServer.on("connection", (socket, request) => {
   console.log("someone connected");
-
-  setTimeout(() => {
-    mongoose.createConnection(
-      "mongodb+srv://Remi:TJQvAr9SnEDGU2D@cluster0.43i0s.mongodb.net/Thesis?retryWrites=true&w=majority",
-      {
-        keepAlive: true,
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-      }
-    );
-  }, 5000);
 
   socket.onmessage = (messageEvent) => {
     const message = JSON.parse(messageEvent.data.toString()) as AppMessage;
