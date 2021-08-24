e0e1b4dea93db9a7f99559afcab149b57281d706
RFalowo <Oluremifalowo@gmail.com>
2021-08-24 14:32:12 +0100

    dsfoif

--- src-server/server.ts
+++ src-server/server.ts
@@ -7,21 +7,105 @@ import UserModel from "../src-shared/users.model.js";
 import EmailModel from "../src-shared/email.model.js";
 
 import dotenv from "dotenv";
-// console.log(dotenv);
-dotenv.config();
+
 // console.log(dotenv.config);
-dotenv.config({ path: "app/.env" });
-const uri = process.env.MONGODBCRED;
+// dotenv.config({ path: "app/.env" });
+// const uri = process.env.MONGODBCRED;
 // console.log(uri);
-mongoose.connect(
-  "mongodb+srv://Remi:TJQvAr9SnEDGU2D@cluster0.43i0s.mongodb.net/Thesis?retryWrites=true&w=majority",
-  {
-    keepAlive: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
+mongoose.set("bufferCommands", false);
+const flibby = async () => {
+  await mongoose.connect(
+    "mongodb+srv://Remi:TJQvAr9SnEDGU2D@cluster0.43i0s.mongodb.net/Thesis?retryWrites=true&w=majority",
+    {
+      keepAlive: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    }
+  );
+  //  catch (e) {
+  //   const result = e;
+  //   console.log(result);
+  // }
+
+  // catch ((error)=>  {
+  //     console.log("connect error: ", error);
+  // })
+
+  // .then(() => {
+  console.log(UserModel);
+  const PORT: string | number = process.env.PORT || 5000;
+  const server = express()
+    .use((req, res) => {
+      res.sendFile("../src-server/server.html");
+    })
+    .listen(PORT);
+
+  const wsServer = new ws.Server({ server });
+
+  interface clients {
+    socket: ws;
+    player: Player;
   }
-);
+  const clients: clients[] = [];
+
+  wsServer.on("connection", (socket, request) => {
+    console.log("someone connected");
+
+    socket.onmessage = (messageEvent) => {
+      const message = JSON.parse(messageEvent.data.toString()) as AppMessage;
+      console.log(message);
+      if (isGetOrder(message)) {
+        console.log("message recieved from client");
+
+        UserModel.estimatedDocumentCount().exec((err, count) => {
+          const response: Order = {
+            type: "Ord",
+            data: count % 2 === 0 ? "Sound" : "3D",
+          };
+          socket.send(JSON.stringify(response));
+          console.log("count error: ", err);
+        });
+      }
+      if (message.type === "PlayerConnected") {
+        console.log(message.data.userID);
+        clients.push({
+          socket,
+          player: message.data,
+        });
+      }
+
+      if (message.type === "PlayerUpdate") {
+        console.log("playerupdate");
+        // (async () => {
+        const user = message.data;
+        //let doc = new userSchema
+        let doc = {
+          // _id: mongoose.Types.ObjectId(),
+          participant: user,
+        };
+        //todo ---------------------- add dot env so mongo creds are hidden
+
+        UserModel.findOneAndUpdate({ "participant.userID": user.userID }, doc, {
+          upsert: true,
+        }).exec();
+      }
+
+      if (message.type === "EmailSubmit") {
+        console.log("email submittion");
+        const email = message.data;
+        let doc = {
+          email: email.Email,
+        };
+
+        EmailModel.create(doc);
+      }
+    };
+  });
+};
+
+flibby();
+// })
 
 // setTimeout(() => {
 //   UserModel.estimatedDocumentCount().exec((err, count) => {
@@ -41,74 +125,7 @@ mongoose.connect(
 //     ).skip(random);
 //   });
 // }, 10000);
-const PORT: string | number = process.env.PORT || 5000;
-const server = express()
-  .use((req, res) => {
-    res.sendFile("../src-server/server.html");
-    console.log("listening");
-  })
-  .listen(PORT);
 
-const wsServer = new ws.Server({ server });
-
-interface clients {
-  socket: ws;
-  player: Player;
-}
-const clients: clients[] = [];
-
-wsServer.on("connection", (socket, request) => {
-  console.log("someone connected");
-
-  socket.onmessage = (messageEvent) => {
-    const message = JSON.parse(messageEvent.data.toString()) as AppMessage;
-    console.log(message);
-    if (isGetOrder(message)) {
-      console.log("message recieved from client");
-      UserModel.estimatedDocumentCount().exec((err, count) => {
-        const response: Order = {
-          type: "Ord",
-          data: count % 2 === 0 ? "Sound" : "3D",
-        };
-        socket.send(JSON.stringify(response));
-        console.log("count error: ", err);
-      });
-    }
-    if (message.type === "PlayerConnected") {
-      console.log(message.data.userID);
-      clients.push({
-        socket,
-        player: message.data,
-      });
-    }
-
-    if (message.type === "PlayerUpdate") {
-      console.log("playerupdate");
-      // (async () => {
-      const user = message.data;
-      //let doc = new userSchema
-      let doc = {
-        // _id: mongoose.Types.ObjectId(),
-        participant: user,
-      };
-      //todo ---------------------- add dot env so mongo creds are hidden
-
-      UserModel.findOneAndUpdate({ "participant.userID": user.userID }, doc, {
-        upsert: true,
-      }).exec();
-    }
-
-    if (message.type === "EmailSubmit") {
-      console.log("email submittion");
-      const email = message.data;
-      let doc = {
-        email: email.Email,
-      };
-
-      EmailModel.create(doc);
-    }
-  };
-});
 //keep anarray of clients
 //remove client from array when disconnected (loop to find client)
 // message function to send to all clients from server
